e4d0f84b9e012796d82a8edf9d069a320b68d23f
Taras Chirkov <chirkovtaras@gmail.com>
2021-01-26 14:40:01 +0200

    added mobile header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const navBurder = document.querySelector('.burger');
+const menu = document.querySelector('.side-info');
+const menuLink = document.querySelector('.menu__list');
+
+navBurder.onclick = function() {
+  navBurder.classList.toggle('active');
+  menu.classList.toggle('active');
+  menuLink.classList.toggle('active');
+};
