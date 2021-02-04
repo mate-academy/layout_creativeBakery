0801caee8d01c5fb34b2fb85a0d23b6d6c31df45
chri911 <delightsome@bigmir.net>
2021-02-04 18:06:42 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav');
+const toggler = document.querySelector('.burger__line');
+const link = document.querySelector('.nav__link');
+const map = document.querySelector('.bakershops__img');
+const min = document.querySelector('.bakershops__label--min');
+const max = document.querySelector('.bakershops__label--max');
+
+burger.addEventListener('click', () => {
+  nav.classList.toggle('nav--mobile');
+  toggler.classList.toggle('burger__line--open');
+  link.classList.toggle('nav__link--active');
+});
+
+link.addEventListener('click', () => {
+  link.classList.toggle('nav__link--active');
+});
+
+min.addEventListener('click', () => {
+  map.style.transform = 'scale(0.9)';
+});
+
+max.addEventListener('click', () => {
+  map.style.transform = 'scale(1.1)';
+});
