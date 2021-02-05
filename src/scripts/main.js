6561ff095fadb5b7d3643ec962c074a5d5eb1951
chri911 <delightsome@bigmir.net>
2021-02-05 14:32:29 +0200

    fixed solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,6 +7,10 @@ const link = document.querySelector('.nav__link');
 const map = document.querySelector('.bakershops__img');
 const min = document.querySelector('.bakershops__label--min');
 const max = document.querySelector('.bakershops__label--max');
+const pressTop = document.querySelector('.nav__press--top');
+const pressBottom = document.querySelector('.nav__press--bottom');
+const listTop = document.querySelector('.list--top');
+const listBottom = document.querySelector('.list--bottom');
 
 burger.addEventListener('click', () => {
   nav.classList.toggle('nav--mobile');
@@ -24,4 +28,26 @@ min.addEventListener('click', () => {
 
 max.addEventListener('click', () => {
   map.style.transform = 'scale(1.1)';
+});
+
+pressTop.addEventListener('click', () => {
+  listTop.classList.toggle('list--open');
+  pressTop.style.display = 'none';
+});
+
+pressBottom.addEventListener('click', () => {
+  listBottom.classList.toggle('list--open');
+  pressBottom.style.display = 'none';
+});
+
+listTop.addEventListener('click', (event) => {
+  pressTop.innerText = event.target.innerText;
+  listTop.classList.toggle('list--open');
+  pressTop.style.display = 'block';
+});
+
+listBottom.addEventListener('click', (event) => {
+  pressBottom.innerText = event.target.innerText;
+  listBottom.classList.toggle('list--open');
+  pressBottom.style.display = 'block';
 });
