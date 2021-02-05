5cc905e17cc3c3705decab330fa13bb949cbc854
Vitalii Skrynkovych <vitaliiskrynkovych@gmail.com>
2021-02-05 17:32:50 +0200

    added some adaptive

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const menuBtn = document.querySelector('.toggle');
+const burger = document.querySelector('.burger');
+
+menuBtn.addEventListener('click', (e) => {
+  burger.classList.toggle('burger--active');
+  menuBtn.classList.toggle('toggle--active');
+});
