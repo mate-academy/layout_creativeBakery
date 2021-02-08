9afbe5a74823f08628ce12cbd8b5bec9452cf51c
Taras Chirkov <chirkovtaras@gmail.com>
2021-02-08 20:46:11 +0200

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,5 +7,5 @@ const menuLink = document.querySelector('.menu__list');
 navBurder.onclick = function() {
   navBurder.classList.toggle('burger_active');
   menu.classList.toggle('side-info_active');
-  menuLink.classList.toggle('menu__list_active');
+  menuLink.classList.toggle('menu__list_tablet_active');
 };
