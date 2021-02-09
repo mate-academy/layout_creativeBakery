f74e365d69ca79dc4d79d4745673156d7f141179
Vasiliy Sergeev <the2producer@gmail.com>
2021-02-10 00:51:49 +0200

    adaptive header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const burger = document.querySelector('.burger');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+burger.addEventListener('click', (e) => {
+  mobileMenu.classList.toggle('mobile-menu--open');
+  burger.classList.toggle('burger--open');
+});
