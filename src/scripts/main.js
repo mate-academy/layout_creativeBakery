c56d4c85a062d5d56dc4fdae074a5c89fe4d8057
Smihotur Roman <smikhotur@gmail.com>
2021-02-13 23:57:21 +0200

    Animation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const buttons = document.getElementsByClassName('butt');
+
+const forEach = Array.prototype.forEach;
+
+forEach.call(buttons, (b) => {
+  b.addEventListener('click', addElement);
+});
+
+function addElement(event) {
+  const addDiv = document.createElement('div');
+
+  const mValue = Math.max(this.clientWidth, this.clientHeight);
+
+  const rect = this.getBoundingClientRect();
+
+  const sDiv = addDiv.style;
+
+  const px = 'px';
+
+  sDiv.width = sDiv.height = mValue + px;
+  sDiv.left = event.clientX - rect.left - (mValue / 2) + px;
+  sDiv.top = event.clientY - rect.top - (mValue / 2) + px;
+
+  addDiv.classList.add('pulse');
+  this.appendChild(addDiv);
+}
