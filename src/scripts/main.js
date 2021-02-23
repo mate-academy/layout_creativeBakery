145bfd98ba96a239537168095c78f24db34ffd9d
Ihor Drul <druleger@gmail.com>
2021-02-23 20:04:19 +0200

    Add header responsive. Created burger menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const burgerOpen = document.getElementById('burger');
+const burgerClose = document.getElementById('burger-close');
+const header = document.getElementById('header-mobile');
+const mobileLinks = document.getElementsByClassName('header__nav-mobile-item');
+
+const menuOpen = () => {
+  header.style.display = 'flex';
+};
+
+const menuClose = () => {
+  header.style.display = 'none';
+};
+
+burgerOpen.addEventListener('click', menuOpen);
+burgerClose.addEventListener('click', menuClose);
+[...mobileLinks].map(item => item.addEventListener('click', menuClose));
