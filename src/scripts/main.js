73feec39421afac2813171206d37592bcbf79ce9
Ivan Bilozerov <vaniabelozerov@gmail.com>
2021-02-24 01:23:05 +0200

    add header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const menuToggle = () => {
+  mobileMenu.classList.toggle('mobile--active');
+};
+
+const burgerBtn = document.querySelector('.header__burger');
+const closeBtn = document.querySelector('.mobile__close');
+const navLinks = [...document.querySelectorAll('.nav__link')];
+const mobileMenu = document.querySelector('.mobile');
+
+window.addEventListener('resize', event => {
+  if (window.innerWidth >= 1000) {
+    mobileMenu.classList.remove('mobile--active');
+  }
+});
+
+navLinks.forEach(link => link.addEventListener('click', menuToggle));
+burgerBtn.addEventListener('click', menuToggle);
+closeBtn.addEventListener('click', menuToggle);
