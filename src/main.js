5778b25533b35bd8323b31959596b1613a2bbe9c
Yurii Lisovenko <yriyliso@gmail.com>
2021-04-14 13:59:21 +0300

    first block

---
+++ src/main.js
@@ -0,0 +1,12 @@
+// 'use strict';
+
+// const nav = document.querySelector('.nav');
+// const burger = document.querySelector('.head__burger');
+// const burgerLine = document.querySelector('.head__burger-line');
+// const toggler = document.querySelector('#toggler');
+
+// toggler.addEventListener('click', () => {
+//   nav.classList.toggle('nav--active');
+//   burger.classList.toggle('head__burger--active');
+//   burgerLine.classList.toggle('head__burger-line--active');
+// });
