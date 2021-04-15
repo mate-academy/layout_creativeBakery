ff1aa22e41b7df6734affbf22fccbd314d1c8c6c
victor <victorarytionowmail@gmail.com>
2021-04-15 14:13:51 +0300

    make header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const numList = document.querySelectorAll('.roller__num-list')[0];
+
+const numUp = function() {
+  numList.appendChild(numList.firstElementChild);
+};
+const numDown = function() {
+  numList.prepend(numList.lastElementChild);
+};
+
+document.querySelector('.roller__up-arrow').addEventListener('click', numUp);
+
+document.querySelector('.roller__down-arrow').addEventListener(
+  'click',
+  numDown
+);
