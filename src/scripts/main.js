35536f860f8a9c120e2d90fed09819e79bd4a369
Anastasiia <anastasiya.triumf@gmail.com>
2021-04-15 16:06:01 +0300

    mobile solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const burger = document.getElementById('burger');
+const navMenu = document.getElementById('nav');
+const span1 = document.getElementById('burger__line--1');
+const span2 = document.getElementById('burger__line--2');
+const span3 = document.getElementById('burger__line--3');
+
+burger.addEventListener('click', () => {
+  navMenu.classList.toggle('display-none');
+  span1.classList.toggle('rotate-plus');
+  span3.classList.toggle('rotate-minus');
+  span2.classList.toggle('display-none');
+});
