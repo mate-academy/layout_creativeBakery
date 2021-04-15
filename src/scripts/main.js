cb38b018fff210a22ce60934d5a82d339c518db5
Oleh Senkiv <o.senkiv.v@gmail.com>
2021-04-16 00:12:15 +0300

    Solution-003

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const toggler = document.querySelector('.header__menu-icon');
+const menu = document.querySelector('.header-asidebar__menu');
+
+toggler.addEventListener('click', () => {
+  toggler.classList.toggle('header__menu-icon--active');
+  menu.classList.toggle('header-asidebar__menu--active');
+});
