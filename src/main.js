5c0d306adc1c23c5d158c3a1950d54505ab44b66
Yurii Lisovenko <yriyliso@gmail.com>
2021-04-15 23:58:14 +0300

    no adaptive

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 'use strict';
 
-const burger = document.querySelector('.nav-header__burger');
+const burger = document.querySelector('.nav__burger');
 const callbackTop = document.querySelector('.callback__top');
 const callback = document.querySelector('.callback');
-const burgerLine = document.querySelector('.nav-header__burger-line');
+const burgerLine = document.querySelector('.nav__burger-line');
 const toggler = document.querySelector('#toggler');
 
 toggler.addEventListener('click', () => {
   callbackTop.classList.toggle('callback__top--active');
   callback.classList.toggle('callback--active');
-  burger.classList.toggle('nav-header__burger--active');
-  burgerLine.classList.toggle('nav-header__burger-line--active');
+  burger.classList.toggle('nav__burger--active');
+  burgerLine.classList.toggle('nav__burger-line--active');
 });
