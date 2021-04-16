e8b87a2a0d632e9152e1bed3a3a782d3d0cbc139
Oleh Senkiv <o.senkiv.v@gmail.com>
2021-04-16 17:54:04 +0300

    Solution-006

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,9 @@
 'use strict';
 
 const toggler = document.querySelector('.header__menu-icon');
-const menu = document.querySelector('.header-asidebar__menu');
+const menu = document.querySelector('.header-asidebar__right-menu');
 
 toggler.addEventListener('click', () => {
   toggler.classList.toggle('header__menu-icon--active');
-  menu.classList.toggle('header-asidebar__menu--active');
+  menu.classList.toggle('header-asidebar__right-menu--active');
 });
