6a96f962de33cce2eb013c689aedaf5146580f41
Oleh Senkiv <o.senkiv.v@gmail.com>
2021-04-18 16:16:32 +0300

    Solution-008

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,8 +2,15 @@
 
 const toggler = document.querySelector('.header__menu-icon');
 const menu = document.querySelector('.header-asidebar__right-menu');
+const page = document.querySelector('.page');
+const footer = document.querySelector('.footer');
 
-toggler.addEventListener('click', () => {
+const toggleMenu = () => {
   toggler.classList.toggle('header__menu-icon--active');
   menu.classList.toggle('header-asidebar__right-menu--active');
-});
+  page.classList.toggle('page--fixed');
+  footer.classList.toggle('footer--fixed');
+};
+
+toggler.addEventListener('click', toggleMenu);
+menu.addEventListener('click', toggleMenu);
