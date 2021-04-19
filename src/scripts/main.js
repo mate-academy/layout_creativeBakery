14d75860a932f2cab2fd331976e18de92f23f9ef
Sergey Zubemail <vook1996@gmail.com>
2021-04-19 09:03:57 +0300

    bakery-test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const header = document.querySelector('.header__nav');
+const toggler = document.querySelector('.header__toggler');
+
+toggler.addEventListener('click', () => {
+  header.classList.toggle('header__nav--active');
+  toggler.classList.toggle('header__toggler--active');
+});
