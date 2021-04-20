978b4cfb9c272f287979c95b9923434705023b90
Vyacheslav Sergienko <vjacheslav.sergienko.01@gmail.com>
2021-04-20 13:43:17 +0300

    update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const toggler = document.querySelector('#toggler');
+const nav = document.querySelector('.header__wrapper');
+const navLink = document.querySelectorAll('.nav__link--mobile');
+
+toggler.addEventListener('click', () => {
+  nav.classList.toggle('header__wrapper--active');
+  toggler.classList.toggle('header__burger--active');
+}
+);
+
+for (const link of navLink) {
+  link.addEventListener('click', () => {
+    nav.classList.toggle('header__wrapper--active');
+    toggler.classList.toggle('header__burger--active');
+  });
+}
