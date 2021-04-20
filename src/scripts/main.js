41829aded05426a313b59e182e5c5912edee32df
Anastasiya <anastasiya.triumf@gmail.com>
2021-04-20 14:01:51 +0300

    fixed hover effects, burger menu, added options

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,11 +2,19 @@
 
 const burger = document.getElementById('burger');
 const navMenu = document.getElementById('nav');
+const navList = document.getElementById('nav-list');
 const span1 = document.getElementById('burger__line--1');
 const span2 = document.getElementById('burger__line--2');
 const span3 = document.getElementById('burger__line--3');
 
 burger.addEventListener('click', () => {
+  navMenu.classList.toggle('display-none');
+  span1.classList.toggle('rotate-plus');
+  span3.classList.toggle('rotate-minus');
+  span2.classList.toggle('display-none');
+});
+
+navList.addEventListener('click', () => {
   navMenu.classList.toggle('display-none');
   span1.classList.toggle('rotate-plus');
   span3.classList.toggle('rotate-minus');
