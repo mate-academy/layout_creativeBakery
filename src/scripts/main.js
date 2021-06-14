220ddc2a8bf38cd5432befc2f32110a0d2e071bd
Tetiana Yasko <taniayasko2015@gmail.com>
2021-06-14 22:41:59 +0300

    part of header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const toggler = document.querySelector('.header__burger');
+const navigation = document.querySelector('.menu');
+
+toggler.addEventListener('click', () => {
+  navigation.classList.toggle('menu__shown');
+  toggler.classList.toggle('header__burger--close');
+});
