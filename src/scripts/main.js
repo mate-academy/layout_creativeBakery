384ae8dce8e8db613f67b26db4235c103710a0b5
Tetiana Yasko <taniayasko2015@gmail.com>
2021-06-15 23:05:43 +0300

    bakery block added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const toggler = document.querySelector('.header__burger');
+const toggler = document.querySelector('.nav__burger');
 const navigation = document.querySelector('.menu');
 
 toggler.addEventListener('click', () => {
   navigation.classList.toggle('menu__shown');
-  toggler.classList.toggle('header__burger--close');
+  toggler.classList.toggle('nav__burger--close');
 });
