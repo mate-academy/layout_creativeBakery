59ac588ef050310f890f5d28d786f46aeba32690
Artem Krulykovskyi <krulikovskiy.a@gmail.com>
2021-06-15 10:12:45 +0300

    mobile format done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const menuButton = document.querySelector('.header__menu-button');
+const menu = document.querySelector('.menu');
+
+menuButton.addEventListener('click', () => {
+  menuButton.classList.toggle('header__menu-button--active');
+  menu.classList.toggle('menu--active');
+});
