8d9efa949bd8aa4d154c83b803e3632846295568
stasianich <spechenuy@gmail.com>
2021-06-15 23:59:42 +0300

    4 issues were fixxed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const feedbackForm = document.querySelector('.feedback-form');
+const inputName = document.querySelector('.feedback-form__name-input');
+const inputEmail = document.querySelector('.feedback-form__email-input');
+const inputPhone = document.querySelector('.feedback-form__phone-input');
+const inputMessage = document.querySelector('.feedback-form__message-input');
+
+feedbackForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  inputName.value = '';
+  inputEmail.value = '';
+  inputPhone.value = '';
+  inputMessage.value = '';
+});
