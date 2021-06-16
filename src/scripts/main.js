967b68ef1a95be5afaecdda9e5f1655bee4924d6
Mykola shcherbak <hohotun21121987@gmail.com>
2021-06-16 21:31:23 +0300

    burger

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const navLink = document.querySelectorAll('.nav__link');
+const checkbox = document.querySelector('.nav__checkbox');
+
+navLink.forEach(el => {
+  el.addEventListener('click', () => {
+    checkbox.checked = !checkbox.checked;
+  });
+});
