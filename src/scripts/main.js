abee3e99ecfb13536758aec824c63da5e908fca3
Vitaliy Martynyuk <vitaliymartiniuk16@gmail.com>
2021-06-16 19:56:43 +0300

    Bakery v2 header+main

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const burger = document.querySelector('.header__menu');
+const nav = document.querySelector('.header__nav');
+
+if (burger) {
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('header__menu--active');
+    nav.classList.toggle('header__nav--active');
+  });
+}
