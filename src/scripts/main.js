470732e0b43fb4022b509ca170774fef22c127be
Tetiana Yasko <taniayasko2015@gmail.com>
2021-06-16 14:41:08 +0300

    about block added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,8 +2,14 @@
 
 const toggler = document.querySelector('.nav__burger');
 const navigation = document.querySelector('.menu');
+const menuList = document.querySelector('.menu__nav-list');
 
 toggler.addEventListener('click', () => {
+  navigation.classList.toggle('menu__shown');
+  toggler.classList.toggle('nav__burger--close');
+});
+
+menuList.addEventListener('click', () => {
   navigation.classList.toggle('menu__shown');
   toggler.classList.toggle('nav__burger--close');
 });
