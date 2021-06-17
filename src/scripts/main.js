78c0459a0b576bf4437682b5324aad009fefa283
Tetiana Yasko <taniayasko2015@gmail.com>
2021-06-17 16:08:31 +0300

    comments fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,13 +3,16 @@
 const toggler = document.querySelector('.nav__burger');
 const navigation = document.querySelector('.menu');
 const menuList = document.querySelector('.menu__nav-list');
+const content = document.querySelector('.page__content');
 
 toggler.addEventListener('click', () => {
-  navigation.classList.toggle('menu__shown');
+  navigation.classList.toggle('menu--shown');
   toggler.classList.toggle('nav__burger--close');
+  content.classList.toggle('page--hidden');
 });
 
 menuList.addEventListener('click', () => {
-  navigation.classList.toggle('menu__shown');
+  navigation.classList.toggle('menu--shown');
   toggler.classList.toggle('nav__burger--close');
+  content.classList.toggle('page--hidden');
 });
