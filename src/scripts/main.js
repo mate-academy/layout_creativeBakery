e235df3a3b1bb4ace364c5f5d8c536ab518658c1
Tetiana Yasko <taniayasko2015@gmail.com>
2021-07-17 20:19:35 +0300

    added changing background

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,13 @@ const toggler = document.querySelector('.nav__burger');
 const navigation = document.querySelector('.menu');
 const menuList = document.querySelector('.menu__nav-list');
 const content = document.querySelector('.page__content');
+const prevButtonHeader = document.querySelector('.slider-header__arrow--prev');
+const nextButtonHeader = document.querySelector('.slider-header__arrow--next');
+const headerNumbers = document.querySelectorAll('.slider-header__item');
+const prevButtonMenu = document.querySelector('.slider-menu__arrow--prev');
+const nextButtonMenu = document.querySelector('.slider-menu__arrow--next');
+const menuNumbers = document.querySelectorAll('.slider-menu__item');
+const header = document.querySelector('.header');
 
 toggler.addEventListener('click', () => {
   navigation.classList.toggle('menu--shown');
@@ -16,3 +23,47 @@ menuList.addEventListener('click', () => {
   toggler.classList.toggle('nav__burger--close');
   content.classList.toggle('page--hidden');
 });
+
+let index = 0;
+
+const imagesArray = [
+  'url("/bg-4.1c5965eb.jpg")',
+  'url("/bg-3.ed7fd6f3.jpg")',
+  'url("/bg-1.5b09dd35.png")'];
+
+const activeNumber = (n, array) => {
+  for (const number of array) {
+    number.classList.remove('number-active');
+  }
+  array[n].classList.add('number-active');
+  header.style.backgroundImage = imagesArray[n];
+};
+
+const nextNumber = (array) => {
+  return () => {
+    if (index === array.length - 1) {
+      index = 0;
+      activeNumber(index, array);
+    } else {
+      index++;
+      activeNumber(index, array);
+    }
+  };
+};
+
+const prevNumber = (array) => {
+  return () => {
+    if (index === 0) {
+      index = array.length - 1;
+      activeNumber(index, array);
+    } else {
+      index--;
+      activeNumber(index, array);
+    }
+  };
+};
+
+prevButtonHeader.addEventListener('click', prevNumber(headerNumbers));
+nextButtonHeader.addEventListener('click', nextNumber(headerNumbers));
+nextButtonMenu.addEventListener('click', nextNumber(menuNumbers));
+prevButtonMenu.addEventListener('click', prevNumber(menuNumbers));
