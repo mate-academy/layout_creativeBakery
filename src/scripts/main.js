3708c8c5d6274b9e0db683740e5e0cf139328371
Loza Marusa <marusa.loza@gmail.com>
2021-09-08 22:42:29 +0300

    header, slider-no main 1/4

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const header = document.querySelector('.header');
+const prev = document.querySelector('.button--prev');
+const next = document.querySelector('.button--next');
+
+prev.addEventListener('click', () => {
+  header.classList.add('header1');
+  header.classList.remove('header');
+});
+
+next.addEventListener('click', () => {
+  header.classList.add('header2');
+  header.classList.remove('header1');
+});
