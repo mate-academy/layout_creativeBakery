90b79e250c65743a33d4e538bec20af7ba6800ea
Vitalii Strohush <vitalikstrog2003v@gmail.com>
2021-09-09 17:39:08 +0300

    bakerlab initial commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,31 @@
 'use strict';
+
+const list = document.querySelector('.counter');
+const prev = document.querySelector('.nav-button--up');
+const next = document.querySelector('.nav-button--down');
+
+prev.disabled = true;
+
+let position = 0;
+
+next.addEventListener('click', function() {
+  move(1);
+});
+
+prev.addEventListener('click', function() {
+  move(-1);
+});
+
+function move(shift) {
+  const count = list.children.length;
+
+  position += shift;
+
+  next.disabled = position >= count - 1;
+  prev.disabled = position <= 0;
+
+  position = (position + count) % count;
+  list.children[position].classList.add('counter__item--active');
+  list.children[position - 1].classList.remove('counter__item--active');
+  list.children[position + 1].classList.remove('counter__item--active');
+}
