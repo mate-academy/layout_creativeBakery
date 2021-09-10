a711d433473def9f75c1af4d92ec1f08fe34bd2b
bohdan_kochetov <bodja1208@gmail.com>
2021-09-10 11:56:09 +0300

    ent

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -29,3 +29,22 @@ right.addEventListener('click', function() {
 left.addEventListener('click', function() {
   step(-1);
 });
+
+const mapa = document.querySelector('.bakershop__mapa');
+const plus = document.querySelector('.bakershop__button-zoom--plus');
+const minus = document.querySelector('.bakershop__button-zoom--minus');
+
+plus.addEventListener('click', function() {
+  zoom(10);
+});
+
+minus.addEventListener('click', function() {
+  zoom(-10);
+});
+
+let add = 0;
+
+function zoom(shift) {
+  add += shift;
+  mapa.style.transform = `scale(${100 + add}%)`;
+}
