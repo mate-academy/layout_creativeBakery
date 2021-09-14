b03988570678bfe393637f21417f7b18344ef710
Mariia Torkaienko <mary_dob@hotmail.com>
2021-09-14 17:41:21 +0300

    Creative Bakery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const btnOpen = document.getElementById('menuBtnOpen');
+const btnClose = document.getElementById('menuBtnClose');
+const menu = document.getElementById('menu');
+
+btnOpen.addEventListener('click', () => {
+  menu.classList.toggle('menu--open');
+});
+
+btnClose.addEventListener('click', () => {
+  menu.classList.toggle('menu--open');
+});
