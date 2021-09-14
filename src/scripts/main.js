f76a8de6167374f2c09404de4912ecc32b2ef7cc
Loza Marusa <marusa.loza@gmail.com>
2021-09-14 16:06:01 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,61 @@
 'use strict';
 
 const header = document.querySelector('.header');
-const prev = document.querySelector('.button--prev');
-const next = document.querySelector('.button--next');
+const slides = document.querySelectorAll('.slide');
+const numbers = document.querySelectorAll('.number');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
 
-prev.addEventListener('click', () => {
-  header.classList.add('header1');
-  header.classList.remove('header');
-});
+let activeSlide = 0;
+let activeNum = 0;
 
 next.addEventListener('click', () => {
-  header.classList.add('header1');
-  header.classList.remove('header');
+  activeSlide++;
+  activeNum++;
+
+  if (activeSlide > slides.length - 1) {
+    activeSlide = 0;
+  }
+
+  if (activeNum > numbers.length - 1) {
+    activeNum = 0;
+  }
+
+  setInitialBg();
+  setActiveSlide();
 });
+
+prev.addEventListener('click', () => {
+  activeSlide--;
+
+  if (activeSlide < 0) {
+    activeSlide = slides.length - 1;
+  }
+
+  if (activeNum < 0) {
+    activeNum = numbers.length - 1;
+  }
+
+  setInitialBg();
+  setActiveSlide();
+});
+
+setInitialBg();
+
+function setInitialBg() {
+  header.style.backgroundImage = slides[activeSlide].style.backgroundImage;
+}
+
+function setActiveSlide() {
+  slides.forEach(slide => {
+    slide.classList.remove('active');
+  });
+
+  numbers.forEach(number => {
+    number.classList.remove('active-p');
+  });
+
+  slides[activeSlide].classList.add('active');
+
+  numbers[activeSlide].classList.add('active-p');
+}
