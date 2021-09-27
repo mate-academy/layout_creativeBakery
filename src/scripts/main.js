184f94c7c2ef8cd2d5e324e5fa7f855e6c060d79
Mariia Obraztsova <mariiaobraztsova99@gmail.com>
2021-09-27 19:09:30 +0300

    Added contact-us block

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", handleEvent);
+
+function handleEvent(event) {
+  event.preventDefault();
+  form.reset();
+}
