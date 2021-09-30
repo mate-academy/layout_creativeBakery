9bc0564e30c35dd4d43fc67c48835eac9902a728
Pavel Podlesnyi <podlesnyi.pavel.v@gmail.com>
2021-09-30 19:21:33 +0300

    some fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,3 +6,9 @@ const side = document.querySelector('.side-mobile');
 burger.addEventListener('click', () => {
   side.classList.toggle('side-mobile--active');
 });
+
+const formAddres = document.querySelector('.form-addres');
+
+formAddres.addEventListener('submit', (e) => {
+  e.preventDefault();
+});
