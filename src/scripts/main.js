4f2d4e2d7f75e8a500278f44414b0df460995daa
Pavel Podlesnyi <podlesnyi.pavel.v@gmail.com>
2021-09-30 16:48:35 +0300

    almost made header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const burger = document.querySelector('.burger');
+const side = document.querySelector('.side-mobile');
+
+burger.addEventListener('click', () => {
+  side.classList.toggle('side-mobile--active');
+})
