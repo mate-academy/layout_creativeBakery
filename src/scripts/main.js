f70d86f50977efaf68c78cb4bb1beb324f277600
Dmytro Hubych <dh.hubych@gmail.com>
2021-10-04 19:48:35 +0300

    fixes before add https://swiperjs.com/

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,3 +8,12 @@ forms.forEach(form => {
     form.reset();
   });
 });
+
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu-mobile') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
