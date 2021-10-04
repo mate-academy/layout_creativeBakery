d2a638877a7fc2f42150293643b312f7e168c02a
Pavel Podlesnyi <podlesnyi.pavel.v@gmail.com>
2021-10-04 14:57:25 +0300

    fix menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,5 @@
 'use strict';
 
-const burger = document.querySelector('.burger');
-const side = document.querySelector('.side-mobile');
-
-window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#menu') {
-    side.classList.add('side-mobile--active');
-    burger.classList.add('burger--active');
-  } else {
-    side.classList.remove('side-mobile--active');
-    burger.classList.remove('burger--active');
-  }
-});
-
 const formAddres = document.querySelector('.form-addres');
 const contactsForm = document.querySelector('#contacts__form');
 
