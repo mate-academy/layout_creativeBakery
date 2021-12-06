3f69896a44afcf8cf8ccdb9ad4b70e2c98aa4fac
Roman Shevchuk <14rom.shevchuk@gmail.com>
2021-12-06 12:21:59 +0200

    bakery header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+$(document).ready(function() {
+  $('.header__burger').click(function(event) {
+    $('.header__burger,.header__menu').toggleClass('active');
+    $('.page__body').toggleClass('lock');
+  });
+});
