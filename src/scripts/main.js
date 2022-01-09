b51b92cf886481f3c48ecdabbabb41dad9aa3221
Dmytro Dsypiuk <dimich578ua@gmail.com>
2022-01-09 09:42:31 +0200

    prittier initial edit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,14 +6,14 @@ const menu = document.querySelector('.menu__list-container');
 const notabs = document.querySelectorAll('.notab *');
 const toggleIcon = document.querySelector('.menu__toggle-icon');
 
-togglers.forEach(toggler => {
+togglers.forEach((toggler) => {
   toggler.addEventListener('click', function() {
     page.classList.toggle('page--with-modal');
     menu.classList.toggle('menu__list-container--open');
     toggleIcon.classList.toggle('fa-grip-lines');
     toggleIcon.classList.toggle('fa-times');
 
-    notabs.forEach(element => {
+    notabs.forEach((element) => {
       if (element.getAttribute('tabindex') === '-1') {
         element.toggleAttribute('tabindex');
       } else {
