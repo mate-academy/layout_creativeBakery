11e35ecd759fb4190204272020e274d4baa6f55c
Mfanomutsha Ncube <mfanomutsha@gmail.com>
2022-04-18 14:43:29 +0200

    Changed the layout in the header and added the menu for mobile and tablet

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const menu = document.querySelector('#menuOverlay');
+const navLink = document.querySelectorAll('.nav__link');
+const openMenuIcon = document.querySelector('.header__menu-toggler');
+const closeMenuIcon = document.querySelector('.menu__hide');
+
+function openMenu(e) {
+  menu.classList.add('menu--is-active');
+  e.preventDefault();
+}
+
+function closeMenu(e) {
+  menu.classList.remove('menu--is-active');
+}
+
+openMenuIcon.addEventListener('click', openMenu, true);
+closeMenuIcon.addEventListener('click', closeMenu, true);
+
+navLink.forEach((n) => n.addEventListener('click', closeMenu, true));
