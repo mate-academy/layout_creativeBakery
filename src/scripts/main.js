4038993a3ac8e3189c5b417a1b358bc64e15b95c
Alexandra Protyanova <protyanova22@gmail.com>
2022-04-19 10:36:40 +0300

    fixed scroll for menu-slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#slider') {
+    document.body.classList.add('page--with-menu');
+  } else {
+    document.body.classList.remove('page--with-menu');
+  }
+});
