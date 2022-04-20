4798345dd1672de2daddc61e486e356acbd68f96
rostyslav-pyliak <r.pyliak@gmail.com>
2022-04-20 12:37:58 +0300

    add mobile menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,26 +1,23 @@
 'use strict';
 
-const showMenu = (toggleId, navId) => {
-  const toggle = document.getElementById(toggleId);
-  const nav = document.getElementById(navId);
+/* ======= SHOW MENU ======= */
 
-  if (toggle && nav) {
-    toggle.addEventListener('click', () => {
-      nav.classList.toggle('show-menu');
-    });
-  }
-};
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
 
-showMenu('nav-toggle', 'nav-menu');
+/* ======= MENU VISIBLE ======= */
 
-// Remove menu mobile
-
-const navLink = document.querySelectorAll('.nav__link');
-
-function linkAction() {
-  const navMenu = document.getElementById('nav-menu');
-  // When we click on each nav__link, we remove the show-menu class
-
-  navMenu.classList.remove('show-menu');
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  });
 }
-navLink.forEach(n => n.addEventListener('click', linkAction));
+
+/* ======= MENU HIDDEN ======= */
+
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+  });
+}
