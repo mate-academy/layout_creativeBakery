caa26ebe7bc217cfabade6f0a3e8d2f5e163b492
rostyslav-pyliak <r.pyliak@gmail.com>
2022-04-20 13:53:25 +0300

    fixed links

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,3 +21,15 @@ if (navClose) {
     navMenu.classList.remove('show-menu');
   });
 }
+
+/* ======= REMOVE MENU MOBILE ======= */
+
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction() {
+  const navMenuMobile = document.getElementById('nav-menu');
+  // When we click on each nav__link, we remove the show-menu class
+
+  navMenuMobile.classList.remove('show-menu');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
