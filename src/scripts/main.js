509498194d4006cfef62cd6eeb9ab99e9c169cb6
Oleh Kalynovskyi <oleh.kalynovskyi@nure.ua>
2022-04-29 00:10:59 +0300

    +header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const icon = document.getElementById('header-burger');
+const item1 = document.getElementById('item1');
+const item2 = document.getElementById('item2');
+const item3 = document.getElementById('item3');
+const item4 = document.getElementById('item4');
+
+icon.addEventListener('click', () => {
+  document.getElementById('menu-container').classList.toggle('menu-opened');
+  document.getElementById('body').classList.toggle('page--menu');
+});
+
+item1.addEventListener('click', () => {
+  document.getElementById('menu-container').classList.toggle('menu-opened');
+});
+
+item2.addEventListener('click', () => {
+  document.getElementById('menu-container').classList.toggle('menu-opened');
+});
+
+item3.addEventListener('click', () => {
+  document.getElementById('menu-container').classList.toggle('menu-opened');
+});
+
+item4.addEventListener('click', () => {
+  document.getElementById('menu-container').classList.toggle('menu-opened');
+});
