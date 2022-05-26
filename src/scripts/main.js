445b119292ceeacde91d7f28fb2445f8704a219b
Ihor Kuchin <vedunigor@gmail.com>
2022-05-26 17:39:01 +0300

    Added header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function doHeaderMenu() {
+  const menu = document.querySelector('.header__menu');
+  const menuBtn = document.querySelector('.header__menu-btn');
+
+  menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('header__menu--active');
+    menuBtn.classList.toggle('header__menu-btn--active');
+  });
+}
+
+function startApp() {
+  doHeaderMenu();
+}
+
+startApp();
