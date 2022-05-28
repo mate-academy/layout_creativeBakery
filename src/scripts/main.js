0c18ffc369d6c7ffa4ad640a374a3301fe30dc9b
Ihor Kuchin <vedunigor@gmail.com>
2022-05-28 23:26:46 +0300

    Added intro section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,10 +3,12 @@
 function doHeaderMenu() {
   const menu = document.querySelector('.header__menu');
   const menuBtn = document.querySelector('.header__menu-btn');
+  const pageBody = document.querySelector('.page__body');
 
   menuBtn.addEventListener('click', () => {
     menu.classList.toggle('header__menu--active');
     menuBtn.classList.toggle('header__menu-btn--active');
+    pageBody.classList.toggle('page__body--stop-scroll');
   });
 }
 
