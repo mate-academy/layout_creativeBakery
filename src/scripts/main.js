b60d6ba6e867f8dc8ab6ddbd4b5d435556031ab5
Oleksandr Kotliarov <de.fr09n@gmail.com>
2022-06-20 13:58:02 +0200

    header was adapted for mobile and tablet

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const nav = document.querySelector('.nav');
+const open = document.querySelector('.header__menu--open');
+const close = document.querySelector('.header__menu--close');
+
+open.addEventListener('click', () => {
+  nav.classList.toggle('nav--active');
+});
+
+close.addEventListener('click', () => {
+  nav.classList.remove('nav--active');
+});
+
+document.querySelectorAll('.nav__link').forEach(n => {
+  n.addEventListener('click', () => {
+    nav.classList.remove('nav--active');
+  });
+});
+
+document.querySelectorAll('.logo').forEach(n => {
+  n.addEventListener('click', () => {
+    nav.classList.remove('nav--active');
+  });
+});
