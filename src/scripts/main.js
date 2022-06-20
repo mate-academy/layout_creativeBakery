788a685e9981487dcbc095ab46cc0dad7e9c7a42
fledith <barateon023@gmail.com>
2022-06-20 17:02:31 +0300

    added 2 section and footer, also added animation and hover effects

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  const hash = window.location.hash;
+
+  if (hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
