ca0113b9ad5e4668d481b5ab23fb81ae2feea376
Oleksandr Kotliarov <de.fr09n@gmail.com>
2022-06-21 16:00:20 +0200

    page content was added for all devices

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,20 +6,24 @@ const close = document.querySelector('.header__menu--close');
 
 open.addEventListener('click', () => {
   nav.classList.toggle('nav--active');
+  document.body.classList.add('page__body--with-menu');
 });
 
 close.addEventListener('click', () => {
   nav.classList.remove('nav--active');
+  document.body.classList.remove('page__body--with-menu');
 });
 
 document.querySelectorAll('.nav__link').forEach(n => {
   n.addEventListener('click', () => {
     nav.classList.remove('nav--active');
+    document.body.classList.remove('page__body--with-menu');
   });
 });
 
 document.querySelectorAll('.logo').forEach(n => {
   n.addEventListener('click', () => {
     nav.classList.remove('nav--active');
+    document.body.classList.remove('page__body--with-menu');
   });
 });
