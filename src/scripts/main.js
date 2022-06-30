473c9a4ea60d7514e705f2d6946e72418be40ab5
Danil Hodytskyi <danil.hodytskyi1105@gmail.com>
2022-06-30 17:51:35 +0300

    landing creative bakery done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+AOS.init({
+  disable: false,
+  startEvent: 'DOMContentLoaded',
+  initClassName: 'aos-init',
+  animatedClassName: 'aos-animate',
+  useClassNames: false,
+  disableMutationObserver: false,
+  debounceDelay: 50,
+  throttleDelay: 99,
+
+  offset: 120,
+  delay: 0,
+  duration: 500,
+  easing: 'ease',
+  once: false,
+  mirror: false,
+  anchorPlacement: 'top-bottom',
+});
