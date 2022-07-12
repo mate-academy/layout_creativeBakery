486cd5292eb01b8c45e539a3758c7ee6cb9bddef
Kate <krasnova2912@gmail.com>
2022-07-12 20:12:24 +0300

    Animation On Scroll added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,23 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+// eslint-disable-next-line no-undef
+AOS.init({
+  disable: false,
+  startEvent: 'DOMContentLoaded',
+  initClassName: 'aos-init',
+  animatedClassName: 'aos-animate',
+  useClassNames: false,
+  disableMutationObserver: false,
+  debounceDelay: 50,
+  throttleDelay: 99,
+
+  offset: 120,
+  delay: 0,
+  duration: 500,
+  easing: 'ease',
+  once: false,
+  mirror: false,
+  anchorPlacement: 'top-bottom',
+});
