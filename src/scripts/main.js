82db64e57f0aa8093e1b0af639161d6ea676eda3
Roman Boiar <rmboiar@gmai.com>
2022-07-31 21:48:12 +0200

    add disable scroll when menu is open, and add theme-toggle for change theme after clicking

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+// a bit of experimenting, just for fun
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('theme').addEventListener('change', function() {
+    if (this.checked) {
+      document.body.classList.add('page--change-theme');
+    } else {
+      document.body.classList.remove('page--change-theme');
+    }
+  }, false);
+});
