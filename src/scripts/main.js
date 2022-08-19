7018ba4f14daa3fd6f2a8b77773dc45cdbd5fe30
Kamil Witek <kamilprogramuje@gmail.com>
2022-08-19 11:11:59 +0200

    desktop version done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const menuBtn = document.querySelector('.nav__mobile-btn-link');
+const menu = document.querySelector('.mobile-menu');
+const closeMenu = document.querySelector('.mobile-menu__x');
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle('show-menu');
+  menu.classList.remove('hide-menu');
+});
+
+closeMenu.addEventListener('click', () => {
+  menu.classList.add('hide-menu');
+  menu.classList.remove('show-menu');
+});
