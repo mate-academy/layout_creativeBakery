7030eff9c89f470834a9aba192c0ce06b846cd81
Maksym Storozhuk <hitthemax111@gmail.com>
2022-08-31 23:27:10 +0300

    First version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('nav__header');
+  } else {
+    document.body.classList.remove('nav__header');
+  }
+});
