e6d3c2a035874604da7bd140bc320b3265b26f85
Vladimir Dyblia <vladimirdublya@gmail.com>
2022-09-06 17:07:12 +0300

    added variables

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const chbox = document.getElementById('menu__toggle');
+
+if (chbox.checked) {
+  document.body.classList.add('body--with-menu');
+} else {
+  document.body.classList.remove('body--with-menu');
+}
