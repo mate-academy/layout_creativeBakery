54c4ba17fecbdf7947308bfe7478b0c87316b79a
Vladimir Dyblia <vladimirdublya@gmail.com>
2022-09-14 14:15:06 +0300

    added transition on menu and fixed footer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const chbox = document.querySelector('#menu__toggle');
+const chlink = document.querySelectorAll('.top-actions__list-link');
 
 chbox.addEventListener('change', () => {
   if (chbox.checked) {
@@ -11,3 +12,12 @@ chbox.addEventListener('change', () => {
     document.querySelector('.menu__logo').src = 'https://raw.githubusercontent.com/vladimir-dublya/layout_creativeBakery/bc8eab8d695a2e911be04083298ba6a73952d543/src/images/menulogo.svg';
   }
 });
+
+for (let i = 0; i < chlink.length; i++) {
+  chlink[i].addEventListener('click', () => {
+    document.body.classList.remove('body--with-menu');
+    chbox.checked = false;
+    document.querySelector('.menu__logo').src = 'https://raw.githubusercontent.com/vladimir-dublya/layout_creativeBakery/bc8eab8d695a2e911be04083298ba6a73952d543/src/images/menulogo.svg';
+  });
+}
+
