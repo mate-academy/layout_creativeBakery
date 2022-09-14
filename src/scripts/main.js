50bec27f2f286fa978ad2aff360788d6ea3232b6
bokorzita <104830913+bokorzita@users.noreply.github.com>
2022-09-14 17:47:11 +0200

    .

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,20 +3,18 @@
 const menuIcon = document.getElementById('menu-toggler');
 let isOpen = false;
 let isPink = false;
+const cookie = document.getElementById('cookie');
+const intro = document.getElementById('intro');
+const title = document.getElementById('title');
+const menu = document.getElementById('menu');
+
+menu.style.transition = 'all 200ms ease-out';
+cookie.style.transition = 'all 200ms ease-out';
+intro.style.transition = 'all 200ms ease-out';
+title.style.transition = 'all 200ms ease-out';
+menu.style.transform = 'translateY(-100%)';
 
 function dropdownMenu() {
-  const cookie = document.getElementById('cookie');
-  const intro = document.getElementById('intro');
-  const title = document.getElementById('title');
-  const menu = document.getElementById('menu');
-
-  menu.style.transition = 'all 200ms ease-out';
-  cookie.style.transition = 'all 200ms ease-out';
-  intro.style.transition = 'all 200ms ease-out';
-  title.style.transition = 'all 200ms ease-out';
-
-  menu.style.opacity = '0';
-
   if (isOpen === true) {
     menu.style.transform = 'translateY(-100%)';
     menuIcon.classList.remove('menu--is-open');
@@ -24,18 +22,14 @@ function dropdownMenu() {
     cookie.style.opacity = '1';
     intro.style.opacity = '1';
     title.style.opacity = '1';
-    document.getElementById('content__intro').style.zIndex = '1';
     isOpen = false;
   } else {
+    isOpen = true;
     menu.style.transform = 'translateY(0)';
     menuIcon.classList.remove('menu--is-closed');
     menuIcon.classList.add('menu--is-open');
-    cookie.style.opacity = '0.5';
-    intro.style.opacity = '0';
-    title.style.opacity = '0';
+    menu.classList.add('menu-color');
     menu.style.opacity = '1';
-    document.getElementById('content__intro').style.zIndex = '0';
-    isOpen = true;
   }
 }
 
@@ -51,6 +45,7 @@ function changeTheme() {
     document.querySelector('body').style.backgroundColor = '#eef7ff';
     document.querySelector('header').style.backgroundColor = '#eef7ff';
     menuIcon.style.backgroundColor = '#eef7ff';
+    menu.style.backgroundColor = '#eef7ff';
 
     for (let i = 0; i < headings.length; i++) {
       headings[i].style.color = '#1c1a26';
@@ -62,6 +57,7 @@ function changeTheme() {
     document.querySelector('body').style.backgroundColor = '#ffeefc';
     document.querySelector('header').style.backgroundColor = '#ffeefc';
     menuIcon.style.backgroundColor = '#ffeefc';
+    menu.style.backgroundColor = '#ffeefc';
 
     for (let i = 0; i < headings.length; i++) {
       headings[i].style.color = '#f9512e';
