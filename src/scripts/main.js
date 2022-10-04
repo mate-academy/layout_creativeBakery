06fb58688fdb04a827fcc0b5c3c6b479f8babe32
DimaShm <shmagindmitry@gmail.com>
2022-10-04 19:51:49 +0300

    JavaScript was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,15 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const popups = document.querySelectorAll('.popup__body');
+
+popups.forEach((popup, index) => {
+  popup.addEventListener('click', (event) => {
+    const element = event.target;
+
+    if (element.classList.contains('popup__body')) {
+      window.location.hash = `#product-${index+1}`;
+    }
+  });  
+});
