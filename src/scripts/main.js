184ed4591314a9a77bc764ea37d0da0ac7264d4e
AniaHna <annahnativ2@gmail.com>
2022-10-13 21:45:08 +0300

    added mailto, added links to this bakery in soc. media, added id for menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const menu = document.querySelector('#menu');
+const burger = document.querySelector('#burger');
+// const cross = document.querySelector('#cross');
+
+burger.addEventListener('click', () => {
+  menu.classList.toggle('menu__hidden');
+});
+
+// cross.addEventListener('click', () => {
+//   menu.classList.add('menu__hidden');
+// });
