41dc93fd7223cae329bf9bc9278b704dfbc79b14
AniaHna <annahnativ2@gmail.com>
2022-10-15 22:23:41 +0300

    added menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,12 +2,7 @@
 
 const menu = document.querySelector('#menu');
 const burger = document.querySelector('#burger');
-// const cross = document.querySelector('#cross');
 
 burger.addEventListener('click', () => {
   menu.classList.toggle('menu__hidden');
 });
-
-// cross.addEventListener('click', () => {
-//   menu.classList.add('menu__hidden');
-// });
