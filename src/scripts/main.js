9830a5c2d6631b8089543856b71ad4b69b955ba3
Maksym Tsarenko <makstsar94@gmail.com>
2022-10-26 16:13:21 +0300

    diasable scroll when menu open

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,11 +7,3 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
-
-window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#menu') {
-    document.body.classList.add('page__body--with-menu');
-  } else {
-    document.body.classList.remove('page__body--with-menu');
-  }
-});
