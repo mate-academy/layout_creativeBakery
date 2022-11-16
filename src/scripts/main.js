ece60b29d929b7e7cb7cad0f4b619ed7e1955914
Nurdan Okcu Tanich <nurokcudan@gmail.com>
2022-11-16 20:52:54 +0100

    initial commit with header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const menuButton = document.querySelector('.icon--menu');
+const closeButton = document.querySelector('.icon--close');
+const nav = document.querySelector('.nav');
+
+menuButton.addEventListener('click', () => {
+  nav.classList.add('nav--active');
+  menuButton.classList.add('hidden');
+  closeButton.classList.remove('hidden');
+});
+
+closeButton.addEventListener('click', () => {
+  nav.classList.remove('nav--active');
+  menuButton.classList.remove('hidden');
+  closeButton.classList.add('hidden');
+});
