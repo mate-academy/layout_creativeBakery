bc49d9feb56c4113f9e7bbc000e089700e16d70e
11Olya112 <OlyaBushovska767@gmail.com>
2023-01-26 22:50:01 +0300

    commit message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const body = document.querySelector('.page__body');
+
+function scrollChange() {
+  const hash = window.location.hash;
+
+  if (hash === '#menu') {
+    body.style.overflow = 'hidden';
+  } else {
+    body.style.overflow = '';
+  }
+}
+
+window.onhashchange = scrollChange;
+
+scrollChange();
