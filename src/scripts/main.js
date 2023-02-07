f5d1c9911ffa8e2fa8e9166637ddb667ee21b5bc
domkab <dominykas.bobinas@gmail.com>
2023-02-07 12:49:14 +0200

    1.1.0 products section finished, started about

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,32 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--no-scroll');
   }
 });
+
+const animatedElements = document.querySelectorAll('.stats__box');
+
+function isInView(element) {
+  const windowHeight = window.innerHeight;
+  const elementTop = element.getBoundingClientRect().top;
+  const elementBottom = element.getBoundingClientRect().bottom;
+
+  return (elementTop <= windowHeight) && (elementBottom >= 0);
+}
+
+window.addEventListener('scroll', function() {
+  animatedElements.forEach(function(element) {
+    if (isInView(element)) {
+      element.classList.add('stats__box--animated');
+    }
+  });
+});
+
+window.addEventListener('scroll', function() {
+  const statsElement = document.querySelector('.stats');
+  const windowHeight = window.innerHeight;
+  const elementTop = statsElement.getBoundingClientRect().top;
+  const elementBottom = statsElement.getBoundingClientRect().bottom;
+
+  if ((elementTop <= windowHeight) && (elementBottom >= 0)) {
+    statsElement.classList.add('stats--show');
+  }
+});
