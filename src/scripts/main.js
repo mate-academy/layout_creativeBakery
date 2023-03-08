3f1b23fc6c862f3d5a80e8b7f894220f55321c9a
Anastasiia Tarasova <tarasova0721@gmail.com>
2023-03-08 15:08:08 +0200

    create Creative Bakery lp

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,44 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const scrollElements = document.querySelectorAll('.scroll');
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop
+    <= (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add('animated');
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove('animated');
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.25)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el);
+    }
+  });
+};
+
+window.addEventListener('scroll', () => {
+  handleScrollAnimation();
+});
