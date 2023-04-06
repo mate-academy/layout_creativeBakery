b63727f65848d645862b54c30f55466c5f85a991
mariuszzawierucha <mariusz.zawierucha@gmail.com>
2023-04-07 01:49:32 +0200

    script for menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const toggleBtn = document.querySelector('#toggleBtn');
+const navList = document.querySelector('.nav__list');
+
+toggleBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  navList.classList.toggle('nav__list--opened');
+});
