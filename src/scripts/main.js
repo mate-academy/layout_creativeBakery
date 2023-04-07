813651b89a5d38e9bc0d7116f52fea421be0c46d
mariuszzawierucha <mariusz.zawierucha@gmail.com>
2023-04-07 13:55:54 +0200

    added animated burger icon

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,5 +6,13 @@ const navList = document.querySelector('.nav__list');
 toggleBtn.addEventListener('click', (e) => {
   e.preventDefault();
 
+  e.target.classList.toggle('header__burger--opened');
   navList.classList.toggle('nav__list--opened');
+
+  navList.querySelectorAll('.nav__link').forEach(el => {
+    el.addEventListener('click', () => {
+      e.target.classList.remove('header__burger--opened');
+      navList.classList.remove('nav__list--opened');
+    });
+  });
 });
