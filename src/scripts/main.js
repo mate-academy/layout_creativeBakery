7fd4bbcabe3f5d71369ce8673a3e50750b7b36d1
Hanna Anishyna <ann.986@gmail.com>
2023-04-13 12:29:47 -0700

    fix image and scroll issues

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page--no-scroll');
+  } else {
+    document.body.classList.remove('page--no-scroll');
+  }
+});
