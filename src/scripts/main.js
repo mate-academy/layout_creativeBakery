be67c9ab9959659b54818462e8a8f169a4262577
Daniel Hladkyi <gladkiy1518@gmail.com>
2023-05-28 11:28:56 +0300

    Completed WEB-site

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,34 @@
 'use strict';
+
+const headerButton = document.querySelector('.header__button');
+const aboutUs = document.querySelector('.about-us');
+const contacts = document.querySelector('.footer');
+const nav = document.querySelector('.nav');
+const navLinks = document.querySelectorAll('.nav__link');
+const burgerMenu = document.querySelector('.header__burger-menu');
+const body = document.getElementById('home');
+const orderButtons = document.querySelectorAll('.product__order-button');
+
+headerButton.addEventListener('click', () => {
+  aboutUs.scrollIntoView({ behavior: 'smooth' });
+});
+
+burgerMenu.addEventListener('click', () => {
+  nav.classList.toggle('nav__active');
+  body.classList.toggle('block-scroll');
+  burgerMenu.classList.toggle('header__burger-menu--active');
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    body.classList.toggle('block-scroll');
+    nav.classList.toggle('nav__active');
+    burgerMenu.classList.toggle('header__burger-menu--active');
+  });
+});
+
+orderButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    contacts.scrollIntoView({ behavior: 'smooth' });
+  });
+});
