d4737fc57c7500cf9bad8ac2f9848be4a7b950e6
Daniel Hladkyi <gladkiy1518@gmail.com>
2023-05-30 12:15:51 +0300

    Tried to fix scroll in nav

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const headerButton = document.querySelector('.header__button');
+const header = document.querySelector('.header');
 const aboutUs = document.querySelector('.about-us');
 const contacts = document.querySelector('.footer');
 const nav = document.querySelector('.nav');
@@ -16,6 +17,7 @@ headerButton.addEventListener('click', () => {
 burgerMenu.addEventListener('click', () => {
   nav.classList.toggle('nav__active');
   body.classList.toggle('block-scroll');
+  header.classList.toggle('block-scroll');
   burgerMenu.classList.toggle('header__burger-menu--active');
 });
 
@@ -23,6 +25,7 @@ navLinks.forEach((link) => {
   link.addEventListener('click', () => {
     if (burgerMenu.offsetWidth > 0) {
       body.classList.toggle('block-scroll');
+      header.classList.toggle('block-scroll');
       nav.classList.toggle('nav__active');
       burgerMenu.classList.toggle('header__burger-menu--active');
     }
