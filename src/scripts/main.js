791b709bd045dd149be997a5c10d04b72859c838
Denis Kononenko <deniskononenko88@gmail.com>
2023-06-18 09:18:51 +0300

    add menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const menuOpener = document.querySelector('.header__menu-opener');
+const menu = document.querySelector('.menu');
+const menuCross = document.querySelector('.menu__cross');
+const menuLinks = document.querySelectorAll('.menu__link');
+
+const closeMenu = () => {
+  menu.classList.remove('menu--show');
+  document.body.classList.remove('page__body--with-menu');
+};
+
+menuOpener.addEventListener('click', () => {
+  menu.classList.add('menu--show');
+  document.body.classList.add('page__body--with-menu');
+});
+
+menuCross.addEventListener('click', closeMenu);
+
+menuLinks.forEach(link => {
+  link.addEventListener('click', closeMenu);
+});
