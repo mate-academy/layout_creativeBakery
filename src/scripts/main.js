fb762833f37cb1c0b10b025512022b33b9c8b366
Sergej <sergej161995@gmail.com>
2023-06-24 11:27:00 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const cards = document.querySelectorAll('.product_card');
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('menu__open');
+  } else {
+    document.body.classList.remove('menu__open');
+  }
+});
+
+// eslint-disable-next-line no-undef
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+      }
+    });
+  },
+  {
+    threshold: 0.6,
+  }
+);
+
+cards.forEach((card) => {
+  observer.observe(card);
+});
