fca0db19a9f5348e8b1499d8086fa841edefa1f4
Pavlo Chudin <maks.chudin567@gmail.com>
2023-07-26 01:59:00 +0300

    did a lot blocks and did some adaptive

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const burger = document.querySelector('.header__btn');
+const burgerMenue = document.querySelector('.burger__menue');
+
+burger.addEventListener('click', () => {
+  if (burgerMenue.classList.contains('burger__menue--active')) {
+    burgerMenue.classList.remove('burger__menue--active');
+    burger.classList.remove('active');
+  } else {
+    burgerMenue.classList.add('burger__menue--active');
+    burger.classList.add('active');
+  }
+});
