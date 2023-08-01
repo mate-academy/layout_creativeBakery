7f285db0a15268e78a3290716015f0f030716ec6
Pavlo Chudin <maks.chudin567@gmail.com>
2023-08-02 02:56:27 +0300

    did som change and ready to next steps

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,11 +4,6 @@ const burger = document.querySelector('.header__btn');
 const burgerMenue = document.querySelector('.burger__menue');
 
 burger.addEventListener('click', () => {
-  if (burgerMenue.classList.contains('burger__menue--active')) {
-    burgerMenue.classList.remove('burger__menue--active');
-    burger.classList.remove('active');
-  } else {
-    burgerMenue.classList.add('burger__menue--active');
-    burger.classList.add('active');
-  }
+  burgerMenue.classList.toggle('burger__menue--active');
+  burger.classList.toggle('active');
 });
