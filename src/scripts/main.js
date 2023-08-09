d84fed864a7405f7e1b39fd7c00d3123e13c9861
Pavlo Chudin <maks.chudin567@gmail.com>
2023-08-09 22:42:02 +0300

    did all visual ost chang footer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,8 +2,26 @@
 
 const burger = document.querySelector('.header__btn');
 const burgerMenue = document.querySelector('.burger__menue');
+const links = document.querySelector('.burger__link');
 
 burger.addEventListener('click', () => {
   burgerMenue.classList.toggle('burger__menue--active');
   burger.classList.toggle('active');
 });
+
+links.addEventListener('click', () => {
+  if (links.classList.contains('burger__link')) {
+    burgerMenue.classList.remove('burger__menue--active');
+    burger.classList.remove('active');
+  }
+});
+
+function checkOrientation() {
+  if (window.innerWidth > window.innerHeight) {
+    document.body.classList.add('landscape');
+  } else {
+    document.body.classList.remove('landscape');
+  }
+}
+checkOrientation();
+window.addEventListener('resize', checkOrientation);
