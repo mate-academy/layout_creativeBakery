759958c613d2590dccf6cebf76abda53c9aa9259
Ivan Vashchuk <vaniavaschuk@gmail.com>
2023-08-20 19:35:35 +0800

    Creative bakery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,15 @@
 'use strict';
 
+// eslint-disable-next-line no-unused-vars
 function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+  document.getElementById('myDropdown').classList.toggle('show');
 };
 
-let menu = document.getElementById("myDropdown");
-let links = document.getElementsByClassName("menu__nav__list-item-link");
+const menu = document.getElementById('myDropdown');
+const links = document.getElementsByClassName('menu__nav__list-item-link');
 
-for(let i = 0; i < links.length; i++) {
+for (let i = 0; i < links.length; i++) {
   links[i].addEventListener('click', () => {
-    menu.classList.remove('show')
-  })
+    menu.classList.remove('show');
+  });
 };
-
