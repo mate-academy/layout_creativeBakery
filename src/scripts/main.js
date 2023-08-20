4e19ae0e937dede5e7a54b1b04b61c26c9e9c8d5
Ivan Vashchuk <vaniavaschuk@gmail.com>
2023-08-20 15:19:20 +0800

    Creative bakery landing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+};
+
+let menu = document.getElementById("myDropdown");
+let links = document.getElementsByClassName("menu__nav__list-item-link");
+
+for(let i = 0; i < links.length; i++) {
+  links[i].addEventListener('click', () => {
+    menu.classList.remove('show')
+  })
+};
+
