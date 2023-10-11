e6a31aab034f8c9ef0cdf150d44a2de4f2bc8714
Oleksii Pishchuhin <aliliolek@gmail.com>
2023-10-11 13:21:05 +0200

    initial commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const openMenu = document.getElementById('open-menu');
+const navHidden = document.getElementById('nav-hidden');
+
+document.addEventListener('click', (event) => {
+  if (event.target === openMenu || openMenu.contains(event.target)) {
+    navHidden.style.right = 0;
+  } else {
+    navHidden.style.right = '-100vw';
+  }
+});
+
+let prevScrollPos = window.pageYOffset;
+const nav = document.getElementById('nav');
+
+window.onscroll = () => {
+  const currentScrollPos = window.scrollY;
+
+  if (prevScrollPos > currentScrollPos) {
+    nav.style.top = '0';
+  } else {
+    nav.style.top = '-8%';
+  }
+
+  prevScrollPos = currentScrollPos;
+};
