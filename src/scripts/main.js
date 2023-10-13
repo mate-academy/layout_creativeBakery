f06077b2890d4524d78149d8d1723839a1c99b19
Oleksii Pishchuhin <aliliolek@gmail.com>
2023-10-13 17:28:20 +0200

    moved button, added underline hover

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,18 +2,25 @@
 
 const openMenu = document.getElementById('open-menu');
 const navHidden = document.getElementById('nav-hidden');
+const navContainer = document.getElementById('nav-container');
+let lastScrollTop = 0;
+
+const main = document.querySelector('.main');
+const footer = document.querySelector('.footer');
 
 document.addEventListener('click', (event) => {
   if (event.target === openMenu || openMenu.contains(event.target)) {
     navHidden.style.right = 0;
+
+    main.style.display = 'none';
+    footer.style.display = 'none';
   } else {
     navHidden.style.right = '-100vw';
+
+    main.style.display = 'block';
+    footer.style.display = 'block';
   }
 });
-
-const navContainer = document.getElementById('nav-container');
-
-let lastScrollTop = 0;
 
 window.addEventListener('scroll', () => {
   const currentScrollTop = window.scrollY;
