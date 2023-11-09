c8f74b4386360723b57adf3294e9781baf1e8223
Oleksandr Kosenko <d1izel@ukr.net>
2023-11-09 13:21:21 +0200

    fixed errors part2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+// Unscroll
+const body = document.body;
+const pageMenu = document.querySelector('.page__menu');
+const iconMenu = document.querySelector('.icon--menu');
+
+iconMenu.addEventListener('click', () => {
+  body.classList.add('unscroll');
+});
+
+pageMenu.addEventListener('click', () => {
+  body.classList.remove('unscroll');
+});
