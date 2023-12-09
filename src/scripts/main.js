285ac212c0d498885f320d8f0fb90e276ebdcccd
BoGoBooM <MyPikcha@gmail.com>
2023-12-09 18:37:08 +0200

    page was  created.I hope that there will be no significant mistakes in this work and I will be able to fulfill my one small goal - to hand in the work without edits.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const menu = document.querySelector('.menu');
+const logo = document.querySelector('.header__logo');
+const menuButton = document.querySelector('.header__burger');
+const menuClose = document.querySelectorAll('.menu__link, .menu__item');
+const menuIconOne = document.querySelector('.header__btn--1');
+const menuIconTwo = document.querySelector('.header__btn--2');
+const body = document.querySelector('.page__body');
+
+menuButton.addEventListener('click', toggleMenu);
+
+menuClose.forEach((btn) => {
+  btn.addEventListener('click', hideMenu);
+  logo.addEventListener('click', hideMenu);
+});
+menu.addEventListener('click', hideMenu);
+
+function toggleMenu() {
+  menu.classList.toggle('active');
+  menuButton.classList.toggle('active');
+  body.classList.toggle('lock');
+  menuIconOne.classList.toggle('active');
+  menuIconTwo.classList.toggle('active');
+}
+
+function hideMenu() {
+  menu.classList.remove('active');
+  menuButton.classList.remove('active');
+  body.classList.remove('lock');
+  menuIconOne.classList.remove('active');
+  menuIconTwo.classList.remove('active');
+}
