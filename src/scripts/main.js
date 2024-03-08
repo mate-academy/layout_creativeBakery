2a72ed2b4e31157e7b81e17f06dae4ded4b659a1
Seva Podolskiy <s_podolskiy@live.com>
2024-03-07 23:43:41 -0500

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,30 @@
+// @ts-nocheck
 'use strict';
+
+/* #region hide menu icon on click */
+const icons = document.querySelector('.top-bar__icons');
+const menuIcon = document.querySelector('.top-bar__icon--burger-menu');
+const closeIcon = document.querySelector('.top-bar__icon--close');
+const logoTopBar = document.querySelector('.menu .top-bar__logo-img');
+const menuLinks = document.querySelectorAll(
+  '.nav--aside .nav__item:nth-of-type(-n+3)',
+);
+
+menuIcon?.addEventListener('click', () => {
+  icons.classList.toggle('top-bar__icons--show');
+});
+
+closeIcon?.addEventListener('click', () => {
+  icons.classList.toggle('top-bar__icons--show');
+});
+
+logoTopBar?.addEventListener('click', () => {
+  icons.classList.toggle('top-bar__icons--show');
+});
+
+menuLinks?.forEach(node => {
+  node?.addEventListener('click', () => {
+    icons.classList.toggle('top-bar__icons--show');
+  });
+});
+/* #end hide menu icon on click */
