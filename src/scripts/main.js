63c39aca290325251d186183bf8317b86858bea6
Ivan Vaniuk <ivanvanuk77@gmail.com>
2024-03-14 13:59:47 +0200

    your mesage

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,38 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const menuLink = document.querySelector('.navbar__menu');
+  const menuClose = document.querySelector('.menu__close');
+  const menu = document.querySelector('.menu');
+
+  function openMenu() {
+    menu.style.display = 'block';
+  }
+
+  function closeMenu() {
+    menu.style.display = 'none';
+  }
+
+  menuLink.addEventListener('click', (event) => {
+    event.preventDefault();
+    openMenu();
+  });
+
+  menuClose.addEventListener('click', (event) => {
+    event.preventDefault();
+    closeMenu();
+  });
+
+  const menuLinks = document.querySelectorAll('.menu__link');
+  menuLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault();
+      const targetId = link.getAttribute('href');
+      const targetSection = document.querySelector(targetId);
+      if (targetSection) {
+        closeMenu();
+        targetSection.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  });
+});
