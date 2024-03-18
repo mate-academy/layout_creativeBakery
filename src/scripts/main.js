a36f37c36290c84279bc6049d1df831ec59b6f73
Oleksii Pishchuhin <aliliolek@gmail.com>
2024-03-18 14:39:13 +0100

    fixed some spacing on  main block

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,34 +1,34 @@
-'use strict';
+"use strict";
 
-const openMenu = document.getElementById('open-menu');
-const navHidden = document.getElementById('nav-hidden');
-const navContainer = document.getElementById('nav-container');
+const openMenu = document.getElementById("open-menu");
+const navHidden = document.getElementById("nav-hidden");
+const navContainer = document.getElementById("nav-container");
 let lastScrollTop = 0;
 
-const main = document.querySelector('.main');
-const footer = document.querySelector('.footer');
+const main = document.querySelector(".main");
+const footer = document.querySelector(".footer");
 
-document.addEventListener('click', (event) => {
+document.addEventListener("click", (event) => {
   if (event.target === openMenu || openMenu.contains(event.target)) {
     navHidden.style.right = 0;
 
-    main.style.display = 'none';
-    footer.style.display = 'none';
+    main.style.display = "none";
+    footer.style.display = "none";
   } else {
-    navHidden.style.right = '-100vw';
+    navHidden.style.right = "-100vw";
 
-    main.style.display = 'block';
-    footer.style.display = 'block';
+    main.style.display = "flex";
+    footer.style.display = "block";
   }
 });
 
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
   const currentScrollTop = window.scrollY;
 
   if (currentScrollTop > lastScrollTop) {
-    navContainer.style.top = '-95px';
+    navContainer.style.top = "-95px";
   } else {
-    navContainer.style.top = '0';
+    navContainer.style.top = "0";
   }
   lastScrollTop = currentScrollTop;
 });
