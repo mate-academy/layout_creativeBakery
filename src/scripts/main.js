2226848d6fe4c02a3bb3b82b700ce5bf7bd891a0
KatsuboDmytro <dmitriyofficial03@gmaik.com>
2024-03-20 12:22:52 +0200

    improve landing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const menuListItems = document.querySelectorAll('.nav__list');
+const navListItems = document.querySelectorAll('.nav__list');
+const menuListItems = document.querySelectorAll('.menu__list');
 const closeButton = document.querySelector('.icon--close');
 const menuButton = document.querySelector('.icon--menu');
 
@@ -15,10 +16,28 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('.page').classList.remove('lock-scrollbar');
   });
 
+  navListItems.forEach(function(item) {
+    item.addEventListener('click', function() {
+      document.querySelector('.page').style.overflow = 'auto';
+      document.querySelector('.page').classList.remove('lock-scrollbar');
+    });
+  });
+
   menuListItems.forEach(function(item) {
     item.addEventListener('click', function() {
       document.querySelector('.page').style.overflow = 'auto';
       document.querySelector('.page').classList.remove('lock-scrollbar');
     });
   });
+});
+
+const imageElement = document.querySelector('.creative__cookie');
+const button = document.querySelector('.button');
+
+button.addEventListener('mouseover', () => {
+  imageElement.src = '/Icon-cookie-white.bb0f611b.svg'; // Update to new image path
+});
+
+button.addEventListener('mouseout', () => {
+  imageElement.src = '/Icon-cookie.72c0e390.svg'; // Revert to original image path
 });
