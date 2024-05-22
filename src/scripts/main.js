263c3b6b9a7bfe63518b1aa8f839335d1dc81fb6
Hanna <hmatviienko@gmail.com>
2024-05-22 19:42:52 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const navLink = document.querySelector('a[href="#nav"]');
+const navCloseBtn = document.querySelector('.header__cover--top--nav--top--close');
+const body = document.body;
+
+navLink.addEventListener('click', () => {
+  console.log('Open');
+  body.classList.add('no-scroll');
+});
+
+navCloseBtn.addEventListener('click', () => {
+  console.log('Close');
+  body.classList.remove('no-scroll');
+});
