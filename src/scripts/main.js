f5a0e0e32454f52ac76981e3bcde6f29111b3569
SrTrace <levashov04sergey@gmail.com>
2024-05-23 13:03:11 +0300

    add layout from mockup and styles

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+// const cards = document.querySelectorAll('.card');
+
+// window.addEventListener('scroll', checkCards);
+
+// checkCards();
+
+// function checkCards() {
+//  const triggerBottom = window.innerHeight / 5 * 4;
+//  cards.forEach(card => {
+//    const cardTop = card.getBoundingClientRect().top;
+//    console.log('triggerBottom: ', triggerBottom);
+// console.log('cardTop: ', cardTop);
+//   if (cardTop < triggerBottom) {
+//    card.classList.add('card--show');
+//   } else {
+//    card.classList.remove('card--show');
+//   }
+//  });
+// }
