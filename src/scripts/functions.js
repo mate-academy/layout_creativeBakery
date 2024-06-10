3787b6e7830f3d21b994559903023e8002e0dd6d
Yevhen Podobedo <yevhen.podobedo@gmail.com>
2024-06-10 22:42:39 +0300

    final solution

--- src/scripts/functions.js
+++ src/scripts/functions.js
@@ -22,30 +22,22 @@ export function classHtml(nameClass, event, newClass) {
 }
 // #endregion
 
-// #region miving and position elements ????????????????????
-  export function trackScreenSize(breakpoint, sec, elem) {
-    if (window.innerWidth >= breakpoint(breakpoint)) {
-      changeThePositionOfTheElement(sec, elem);
-    }
-  }
-
-  export function changeThePositionOfTheElement(section, element) {
-    return querySelector(section).appendChild(querySelector(element));
-  };
-// #endregion
-
 // #region template for product setion
 export function templateHtmlForProduct(name) {
   return  `<article class="products__card">
+
+            <div class="products__image">
             <img
               class="products__img"
               src=${name.image}
               alt="Creative Bakery cakes"
             >
+            </div>
+
             <h3 class="products__subtitle">${name.title}</h3>
             <p class="products__text">${name.text}</p>
             <div class="products__wrapper">
-            <a class="products__link" href=${name.link} target="_blank">Order</a>
+            <a class="products__link" href=${name.link}>Order</a>
             </div>
           </article>`;
 };
@@ -55,5 +47,45 @@ export function templateHtmlForAboutUs(name) {
             <h2 class="about-us__numbers">${name.numbers}</h2>
             <p class="about-us__text">${name.text}</p>
           </div>`;
-}
+};
+
+export function templateHtmlInformationNotification(nameClass, content, method, url) {
+
+  if(querySelector(nameClass)) {
+    return;
+  }
+
+  const template = document.createElement('article');
+        template.classList = nameClass;
+
+        template.innerHTML = `
+          <img
+            class="${nameClass}__icon"
+            src="${url}"
+            alt="reative Bakery phone"
+          >
+
+          <div class="${nameClass}__dots">
+            <span>.</span>
+            <span>.</span>
+            <span>.</span>
+          </div>
+        `;
+
+        setTimeout ( ()=> {
+          template.style.transform = 'scale(1)';
+        }, 10);
+
+        setTimeout ( ()=> {
+          template.style.transform = 'scale(0)';
+        }, 5000);
+
+        setTimeout ( ()=> {
+          template.remove();
+        }, 5200);
+
+  return (method === 'first')
+  ? querySelector(content).prepend(template)
+  : querySelector(content).appendChild(template);
+};
 // #endregion
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,34 +4,74 @@ import { infoBlock, products } from "./array_of_data.js";
 
 import {
   classHtml,
-  cloningElement,
-  elementCloning,
-  elementCloningAll,
   querySelector,
   querySelectorAll,
   templateHtmlForAboutUs,
   templateHtmlForProduct,
-  trackScreenSize
+  templateHtmlInformationNotification
 } from "./functions.js";
 
 const ACTIVE = '--active';
 const NO_ACTIVE = '--no-active';
 
-//#region button menu
+//#region menu
+
+// button menu
   querySelector('top-bar__icon-menu').addEventListener('click', ()=> {
 
     classHtml('top-bar__icon-menu', 'toggle', 'top-bar__icon-menu' + ACTIVE);
     classHtml('menu', 'toggle', 'menu' + ACTIVE);
+    // classHtml('menu__contact', 'add', 'menu__contact' + ACTIVE);
+
     (querySelector('menu--active'))
       ? classHtml('body', 'add', 'body__lock')
       : classHtml('body', 'remove', 'body__lock');
+  });
 
-    });
-    // #endregion
+  // menu window
+  querySelector('menu__list').addEventListener('click', () => {
+    classHtml('body', 'remove', 'body__lock');
+    classHtml('top-bar__icon-menu', 'remove', 'top-bar__icon-menu' + ACTIVE);
+    classHtml('menu', 'remove', 'menu' + ACTIVE);
+  });
 
-    querySelector('products__content').innerHTML = products.map(templateHtmlForProduct).join('');
+// #endregion
 
-    querySelector('about-us__content').innerHTML = infoBlock.map(templateHtmlForAboutUs).join('');
+  querySelector('products__content').innerHTML = products.map(templateHtmlForProduct).join('');
+
+  querySelector('about-us__content').innerHTML = infoBlock.map(templateHtmlForAboutUs).join('');
+
+// #region view information notification
+  querySelector('menu__contact').addEventListener('click', () => {
+    templateHtmlInformationNotification('popup-call', 'menu__contact', 'first', '/icon-phone.39dbd526.svg');
+
+    classHtml('menu__contact--icon', 'add', 'menu__contact--icon' + ACTIVE);
+
+    setTimeout( ()=> {
+      classHtml('menu__contact--icon', 'remove', 'menu__contact--icon' + ACTIVE);
+    }, 5200);
+  });
+
+  querySelector('footer__call-list').addEventListener('click', () => {
+
+    templateHtmlInformationNotification(
+      'popup-call',
+      'footer__content-contact',
+      'end',
+      '/icon-phone.39dbd526.svg'
+    );
+  });
+
+  querySelector('footer__mail').addEventListener('click', () => {
+
+    templateHtmlInformationNotification(
+      'popup-call',
+      'footer__content-email',
+      'end',
+      '/icon-mail.42c4e05a.svg'
+    );
+  });
+// #endregion
 
     // #region clone navigation menu
     const cloneMenu = querySelector('menu__list').cloneNode(true);
@@ -49,4 +89,17 @@ const NO_ACTIVE = '--no-active';
     for(let i of cloneLink) {
       i.classList = 'footer__link';
     };
+
+    // add new position menu
+    const newfooterItem = document.createElement('LI');
+    const newfooterLink= document.createElement('A');
+
+    newfooterItem.className = 'footer__item';
+
+    newfooterLink.href = '#';
+    newfooterLink.classList = 'footer__link';
+    newfooterLink.textContent = 'Terms & Privacy';
+
+    newfooterItem.appendChild(newfooterLink);
+    cloneMenu.appendChild(newfooterItem);
     // #endregion
