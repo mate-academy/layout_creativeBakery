32c2fc6379ca1e1ae33d1447c5ca48ff303f3e76
Your_Name Your_Surname <your@email>
2024-07-24 14:27:29 +0300

    add sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,11 @@
 window.addEventListener('hashchange', (event) => {
-  const footer = document.querySelector('footer'); // Замініть 'footer' на селектор вашого футера
+  const footer = document.querySelector('footer');
 
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
-    footer.style.display = 'none'; // Приховати футер
+    footer.style.display = 'none'; 
   } else {
     document.body.classList.remove('page__body--with-menu');
-    footer.style.display = ''; // Відновити футер
+    footer.style.display = '';
   }
 });
