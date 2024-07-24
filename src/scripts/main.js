5de55587a6f0ceb7d3e8845d8c1922d9ec2f5193
Your_Name Your_Surname <your@email>
2024-07-24 14:16:14 +0300

    add last solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,11 @@
-'use strict';
-window.addEventListener('hashchange', () => {
+window.addEventListener('hashchange', (event) => {
+  const footer = document.querySelector('footer'); // Замініть 'footer' на селектор вашого футера
+
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
+    footer.style.display = 'none'; // Приховати футер
   } else {
     document.body.classList.remove('page__body--with-menu');
+    footer.style.display = ''; // Відновити футер
   }
 });
