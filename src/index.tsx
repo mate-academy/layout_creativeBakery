825e3c75d17e1cad490e3d1409aa536c63334a16
iryna lysko <goiralysko@gmail.com>
2024-09-21 10:20:29 +0300

    fix mistake

--- src/index.tsx
+++
@@ -1,7 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import 'bulma/css/bulma.css';
-import '@fortawesome/fontawesome-free/css/all.css';
-
-import { Root } from './Root';
-
-createRoot(document.getElementById('root') as HTMLDivElement).render(<Root />);
