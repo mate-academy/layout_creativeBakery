9172817ce766f484d2d761226100f29ca9a91a00
Nadiia Bulmak <nadgaval@gmail.com>
2025-01-10 09:26:09 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,47 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const cards = document.querySelectorAll('.section__card');
+  const section = document.querySelector('.section__what-we-bake');
+
+  const observer = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach((entry, index) => {
+        if (entry.isIntersecting) {
+          setTimeout(() => {
+            entry.target.classList.add('animate');
+          }, index * 300);
+        }
+      });
+    },
+    { threshold: 0.5 }
+  );
+
+
+  cards.forEach(card => observer.observe(card));
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const menu = document.getElementById("menu");
+  const openMenuButton = document.querySelector(".icon--menu");
+  const closeMenuButton = document.querySelector(".icon--close");
+  const body = document.body;
+  const menuLinks = document.querySelectorAll("#menu a");
+
+  openMenuButton.addEventListener("click", () => {
+    menu.classList.add("menu--open");
+    body.classList.add("no-scroll");
+  });
+
+  closeMenuButton.addEventListener("click", () => {
+    menu.classList.remove("menu--open");
+    body.classList.remove("no-scroll");
+  });
+
+  menuLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      menu.classList.remove("menu--open");
+      body.classList.remove("no-scroll");
+    });
+  });
+});
