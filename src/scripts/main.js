881b470495219feee42c61f4f232f2ed4857ac89
diachkinainna <diachkinainna@gmail.com>
2025-01-15 16:22:35 +0200

    add basic solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const body = document.querySelector('.page__body');
+const open = document.querySelector('.top-bar__icon--menu');
+const close = document.querySelector('.top-bar__icon--close');
+
+open.addEventListener('click', () => {
+  body.classList.add('no-scroll');
+})
+
+close.addEventListener('click', () => {
+  body.classList.remove('no-scroll');
+})
