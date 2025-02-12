a74fd4ee9fb09d1d627ae300184cff7ebff6f82d
Oleg Orykhivskiy <olegkoolll@gmail.com>
2025-02-12 15:48:10 +0200

    done2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+window.addEventListener('load', () => {
+  const loadAnimation = document.querySelector('.load__animation');
+  const mainContent = document.querySelector('.main__content')
+
+  loadAnimation.style.display = 'none'
+
+  mainContent.classList.remove('hiden');
+  mainContent.classList.add('show');
+})
