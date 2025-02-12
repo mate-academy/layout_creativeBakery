de0951ba081cc7c17f60e7231beb4397a190563b
Arthur Ogunfuye <arthurity@yahoo.co.uk>
2025-02-12 09:10:30 +0000

    Creative Bakery solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const body = document.querySelector('.page__body');
+const open = document.querySelector('.top-bar__icon--menu');
+const close = document.querySelector('.top-bar__icon--close');
+
+open.addEventListener('click', () => {
+  body.classList.add('no-scroll');
+});
+
+close.addEventListener('click', () => {
+  body.classList.remove('no-scroll');
+});
+
+const productCards = document.querySelectorAll('.product-card');
+
+window.addEventListener('scroll', () => {
+  const scrollPosition = window.scrollY + window.innerHeight;
+  productCards.forEach((card) => {
+    const sectionPosition = card.offsetTop;
+
+    if (scrollPosition > sectionPosition) {
+      card.classList.add('product-card--show');
+    }
+  });
+});
