69b6364a77f18a023b2ed700d651242b4eb96f80
Oleg Orykhivskiy <olegkoolll@gmail.com>
2025-02-12 16:36:30 +0200

    anim3

--- src/scripts/animationLoad.js
+++ src/scripts/animationLoad.js
@@ -1,13 +1,32 @@
 'use strict'
 
-window.addEventListener('load', () => {
+window.addEventListener('DOMContentLoaded', () => {
+  // Створюємо основні елементи до завантаження сторінки
+  const body = document.body;
+
+  // Створюємо блок анімації
+  const loadAnimation = document.createElement('div');
+  loadAnimation.classList.add('load__animation');
+
+  // Створюємо круг для анімації
+  const circle = document.createElement('div');
+  circle.classList.add('circle');
+
+  // Додаємо круг в блок анімації
+  loadAnimation.appendChild(circle);
+
+  // Додаємо блок анімації в body
+  body.appendChild(loadAnimation);
+
+  // Затримка для анімації, яка триває 1.5 секунди перед показом контенту
   setTimeout(() => {
-  const loadAnimation = document.querySelector('.load__animation');
-  const mainContent = document.querySelector('.main__content')
+    const mainContent = document.querySelector('.main__content');
 
-  loadAnimation.style.display = 'none'
+    // Сховуємо анімацію завантаження
+    loadAnimation.style.display = 'none';
 
-  mainContent.classList.remove('hiden');
-  mainContent.classList.add('show');
-}, 1500)
-})
+    // Показуємо основний контент
+    mainContent.classList.remove('hiden');
+    mainContent.classList.add('show');
+  }, 1500); // Час анімації
+});
