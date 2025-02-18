daf4cc5b67604309e61b1a47d14259d89665e078
Oleg Orykhivskiy <olegkoolll@gmail.com>
2025-02-18 22:57:38 +0200

    Fix problem

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const headerMenu = document.querySelector('.header__menu');
+const asideMenu = document.querySelector('.aside__menu')
+const section = document.querySelector('.section')
+const body = document.querySelector('body')
+
+headerMenu.addEventListener('click', () => {
+  body.classList.add('menu__active');
+  section.classList.add('section__display-none')
+})
+
+asideMenu.addEventListener('click', () => {
+  body.classList.remove('menu__active');
+  section.classList.remove('section__display-none')
+})
