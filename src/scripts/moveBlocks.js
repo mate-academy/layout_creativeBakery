cc4c4ba0d8d05e3d12f2ed724e1cde2f51cdda24
Oleg Orykhivskiy <olegkoolll@gmail.com>
2025-02-20 14:10:36 +0200

    Fix problem 5

--- src/scripts/moveBlocks.js
+++ src/scripts/moveBlocks.js
@@ -6,7 +6,12 @@ function isElementInViewport(el) {
   return rect.top >= 0 && rect.top <= window.innerHeight;
 }
 
+let ignoreAnimation = false;
+
 window.addEventListener('scroll', () => {
+
+  if (ignoreAnimation) return;
+
   const boxesLeft = document.querySelectorAll('.left');
   const boxesRight = document.querySelectorAll('.right')
 
@@ -24,18 +29,53 @@ window.addEventListener('scroll', () => {
   });
 });
 
-//target menu 
+//target menu
 const headerMenu = document.querySelector('.header__menu');
 const asideMenu = document.querySelector('.aside__menu')
+const asideBlock = document.querySelector ('.aside')
 const section = document.querySelector('.section')
 const body = document.querySelector('body')
+const anchorLinks = document.querySelectorAll('.aside__content a[href^="#"]');
 
 headerMenu.addEventListener('click', () => {
   body.classList.add('menu__active');
   section.classList.add('section__display-none')
+  asideBlock.classList.add('aside--open')
+
 })
 
 asideMenu.addEventListener('click', () => {
   body.classList.remove('menu__active');
   section.classList.remove('section__display-none')
+  asideBlock.classList.remove('aside--open')
 })
+
+// Відновлення прокрутки після натискання на посилання в меню
+anchorLinks.forEach(link => {
+  link.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    ignoreAnimation = true;
+
+        // Закриваємо меню і відновлюємо скрол
+        body.classList.remove('menu__active');
+        section.classList.remove('section__display-none');
+        asideBlock.classList.remove('aside--open')
+
+    const targetSectionId = link.getAttribute('href').substring(1);
+    const targetSection = document.getElementById(targetSectionId);
+
+    // Прокручуємо сторінку до потрібної секції
+    setTimeout(() => {
+      targetSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+
+      // Встановлюємо фокус на цей елемент
+      targetSection.focus();
+    }, 100);
+
+    // Відновлюємо анімацію після прокручування
+    setTimeout(() => {
+      ignoreAnimation = false;
+    },200);
+  });
+});
