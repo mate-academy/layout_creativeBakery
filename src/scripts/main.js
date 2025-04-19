961466bb3634d9d43377a1f334f7b440fff261ca
Denis Pitsul <denia.pitsul@gmail.com>
2025-04-19 12:33:02 +0300

    add creative bakery landing solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const page = document.querySelector('.page');
+const switcher = document.querySelector('.theme-switcher');
+
+switcher.addEventListener('click', () => {
+  if (page.classList.contains('page--theme--pink')) {
+    page.classList.remove('page--theme--pink');
+    switcher.classList.remove('theme-switcher--theme--pink');
+  } else {
+    page.classList.add('page--theme--pink');
+    switcher.classList.add('theme-switcher--theme--pink');
+  }
+});
