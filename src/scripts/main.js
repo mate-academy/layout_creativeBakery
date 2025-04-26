c4b7244dda0687ef2d3e7718332a5ee439357373
Ralfendeck25 <poseidoncenghizerzhemetmhossad@outlook.com>
2025-04-26 06:24:37 -0300

    m623

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
 'use strict';
+
+  function handleScroll() {
+    const topBar = document.querySelector('.top-bar-header');
+    if (window.scrollY > 0) {
+      topBar.classList.add('scrolled');
+    } else {
+      topBar.classList.remove('scrolled');
+    }
+  }
+
+window.addEventListener('scroll', handleScroll);
+
+function changeBackgroundColor() {
+  const pageBody = document.querySelector('.page__body');
+  const currentColor = getComputedStyle(pageBody).getPropertyValue('background-color').trim();
+
+  if (currentColor === 'rgb(255, 238, 252)') {
+    pageBody.style.backgroundColor = '#EEF7FF';
+  } else {
+    pageBody.style.backgroundColor = '#FFEEFC';
+  }
+}
+
+setInterval(changeBackgroundColor, 5000);
