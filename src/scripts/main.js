a04e25d3a577194a4cc26aa961072e1f8afe1c07
Viktoriia <vkkishko@gmail.com>
2025-04-26 19:56:32 +0300

    first draft lamding

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const body = document.body;
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav');
+const header = document.querySelector('.header');
+
+burger.addEventListener('click', () => {
+  header.classList.toggle('header--canTransform');
+  burger.classList.toggle('burger--canTransform');
+  nav.classList.toggle('nav--canTransform');
+  body.classList.toggle('no-scroll');
+
+  console.log('hello');
+});
+
+
