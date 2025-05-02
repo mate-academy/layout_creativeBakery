f88db6157d99fca6d75a9a23931e203c326ec90e
Alexandre <alexandredd@duck.com>
2025-05-02 14:19:32 -0300

    add functional header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,37 @@
 'use strict';
+
+const menu = document.querySelector('#menu');
+const menuToggle = document.querySelector('.nav__toggle');
+const menuLinks = document.querySelectorAll('.nav__link');
+
+const toggleMenu = () => {
+  const isMenuOpen = menu.classList.contains('menu-active');
+
+  menu.classList.toggle('menu-active');
+  document.body.style.overflow = isMenuOpen ? 'hidden' : '';
+};
+
+menuLinks.forEach((link, index) => {
+  link.style.setProperty('--index', index);
+});
+
+if (menuToggle) {
+  menuToggle.addEventListener('click', (e) => {
+    e.preventDefault();
+    toggleMenu();
+  });
+}
+
+menuLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    if (menu.classList.contains('menu-active')) {
+      toggleMenu();
+    }
+  });
+});
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape' && menu.classList.contains('menu-active')) {
+    toggleMenu();
+  }
+});
