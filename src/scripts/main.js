2ea05ad73c3d9e13b455a5740dce116d9deb4d91
keygen_exe <matkovskiyf3vlad@gmail.com>
2025-07-01 16:43:09 +0300

    add xmark in burger-menu and changed style of crumbs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,5 +6,6 @@ const body = document.querySelector('body');
 
 burgerMenu.addEventListener('click', () => {
   navMenu.classList.toggle('header__menu--active');
+  burgerMenu.classList.toggle('header__burger_menu--active');
   body.classList.toggle('menu_open');
 });
