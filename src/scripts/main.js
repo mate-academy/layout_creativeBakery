44a03ff9b8838ee9753471b929dc4ac6d353a83f
keygen_exe <matkovskiyf3vlad@gmail.com>
2025-07-01 15:08:19 +0300

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const burgerMenu = document.querySelector('.header__burger_menu');
+const navMenu = document.querySelector('.header__menu');
+const body = document.querySelector('body');
+
+burgerMenu.addEventListener('click', () => {
+  navMenu.classList.toggle('header__menu--active');
+  body.classList.toggle('menu_open');
+});
