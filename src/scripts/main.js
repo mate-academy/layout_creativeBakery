a93f239ff8dd8bc756981d775c154c8b54bf16be
BadOfficer <tarasbondarenko2005@gmail.com>
2025-07-10 19:29:04 +0300

    add ready landing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const burger = document.querySelector('.burger');
+const mobileMenu = document.querySelector('.page__mobile-menu');
+
+burger.addEventListener('click', (e) => {
+  e.target.classList.toggle('burger--close');
+  mobileMenu.classList.toggle('page__mobile-menu--active');
+})
