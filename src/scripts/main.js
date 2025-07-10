5a3ba9588ea9d2d0001e965b0e8c1c174a3a02a0
BadOfficer <tarasbondarenko2005@gmail.com>
2025-07-10 21:53:21 +0300

    fixed bug with mobile menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,8 +2,16 @@
 
 const burger = document.querySelector('.burger');
 const mobileMenu = document.querySelector('.page__mobile-menu');
+const menuLinks = document.querySelectorAll('.menu__link');
 
 burger.addEventListener('click', (e) => {
   e.target.classList.toggle('burger--close');
   mobileMenu.classList.toggle('page__mobile-menu--active');
+})
+
+menuLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    mobileMenu.classList.remove('page__mobile-menu--active');
+    burger.classList.remove('burger--close');
+  })
 })
